581952c7f4e8ff7bd14711d47e20767fbb502b5c
Servat <sajjad.servatjoo@gmail.com>
2024-02-02 13:08:38 +0330

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
-import { Nobitex } from './nobitex';
-
 export * from './currency';
 export * from './nobitex';
 export * from './resolutions';
 export * from './symbols';
 export * from './type-event';
 export * from './types';
-const main = async () => {
-  const n = new Nobitex('sdsd', 'aaa');
-  const a = n.watchTimeFrameOHLCV('BCHIRT', '1m');
-  a.on((data) => {
-    console.log(data, new Date());
-  });
-  // setTimeout(() => {
-  //   a.stop();
-  // }, 8000);
-};
-main();
